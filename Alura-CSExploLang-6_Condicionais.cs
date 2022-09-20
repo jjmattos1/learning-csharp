95309ba943b5243b53fd3a5c624b0e9c5f58a5e3
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-09-20 19:16:32 -0300

    Atualizando do repo local Android do app C# Shell.

--- Alura-CSExploLang-6_Condicionais.cs
+++ Alura-CSExploLang-6_Condicionais.cs
@@ -1,9 +1,30 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-class 6Condiconais
+class P6Condiconais
 {
-	int void Main(string[] args)
+	static void Main(string[] args)
 		{
+		Console.WriteLine("Projeto 6 - Condicionais\n");
+		
+		int idade = 17;
+		int quantidadePessoas = 3;
+		if (idade >= 18)
+			{
+			Console.WriteLine("Parabens, voce pode entrar! \n");
+			}
+		else
+			if (quantidadePessoas >= 3)
+				{
+				Console.WriteLine("Voce nao tem 18 mas, "
+				+ "esta acompanhado e por isso pode entrar!");
+				}
+				else
+				{
+				Console.WriteLine("Infelizmente voce nao pode entrar!");	
+				}
 		
 		}
 }
