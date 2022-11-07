4079045809b88d7fbd4d32388d28f06e520c5340
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-11-07 18:20:12 -0300

    Exercicio DES Fatorial em andamento.

--- vs2022/AluraCSExploLang/DES-Fatorial/Program.cs
+++ vs2022/AluraCSExploLang/DES-Fatorial/Program.cs
@@ -8,7 +8,29 @@ class DES_Fatorial
 
         Console.WriteLine("Desafio - Imprimir fatoriais de 1 a 10\n");
 
+        int fato, fatoBase, repFato, repFato1, fatoAtual;
+        fato = fatoBase = repFato = repFato1 = fatoAtual = 1;
         
+
+        for ( repFato = 1; repFato <= 10; repFato++ )
+        {
+            if ( repFato = 1 )
+                Console.WriteLine("O fatorial de 1 é 1.");
+            else
+            {
+                fatoAtual++;
+                
+                Console.WriteLine("O fatorial de "+fatoAtual+" é: ");
+                for ( repFato1 = 1 ; repFato1 <= fatoAtual ; repFato1++ )
+                {
+                    fato = repFato1 * fatoAtual;
+                    Console.Write(fato);
+                    Console.WriteLine("\n");
+                }
+
+                //fato++;
+            }
+        }
 
         // closing...
         Console.WriteLine("\n\nPress any key to continue...");
