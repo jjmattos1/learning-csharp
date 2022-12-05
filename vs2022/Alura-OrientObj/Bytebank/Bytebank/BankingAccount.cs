1ac57a847c64c71c03edcea1c01eb83aa1b6a376
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-12-05 19:46:20 -0300

    Atualizando repo Windows.

--- vs2022/Alura-OrientObj/Bytebank/Bytebank/BankingAccount.cs
+++ vs2022/Alura-OrientObj/Bytebank/Bytebank/BankingAccount.cs
@@ -10,8 +10,9 @@ namespace Bytebank
     {
         public int agNum = 0;
         public string accNum = "";
-        public string owner = "";
+        //public string owner = "";
         public double balance = 0.00;
+        public Client owner;
 
         public void Withdrawn(double value)
         {
@@ -33,12 +34,20 @@ namespace Bytebank
             }
             else
             {
-                this.balance -= value;
-                destiny.balance += value;
+                this.Withdrawn(value);
+                destiny.Deposit(value);
                 return true;
 
             }
 
+        }
+
+        public void ShowAcc(BankingAccount selAcc)
+        {
+            Console.WriteLine("\nAgência: "+selAcc.agNum);
+            Console.WriteLine("Num. conta: "+selAcc.accNum);
+            Console.WriteLine("Titular: "+selAcc.owner);
+            Console.WriteLine("Saldo: "+selAcc.balance);
         }
     }
 }
--- vs2022/Alura-OrientObj/Bytebank/Bytebank/Program.cs
+++ vs2022/Alura-OrientObj/Bytebank/Bytebank/Program.cs
@@ -2,14 +2,14 @@
 using System;
 using System.Linq;
 // line
+// github rep deste curso https://github.com/alura-cursos/curso_OrientacaoObjetosC01R/tree/main
+
 BankingAccount AndreAccount = new BankingAccount();
 
 AndreAccount.owner = "André Silva";
 AndreAccount.agNum = 15;
 AndreAccount.accNum = "1010-X";
 AndreAccount.balance = 100;
-
-Console.WriteLine("Saldo da conta do André: R$ "+AndreAccount.balance);
 
 //conta default
 BankingAccount conta = new BankingAccount();
@@ -48,8 +48,36 @@ Console.WriteLine($"\nSaldo da conta da Maria é: R$ {String.Format("{0:0.00}", 
 
 AndreAccount.Transfer(50, MariaAccount);
 
-Console.WriteLine($"\nSaldo da conta da Maria, apos transf. da conta do Andre: R$ {String.Format("{0:0.00}", MariaAccount.balance)}");
-Console.WriteLine($"E o saldo da conta do Andre: R$ {String.Format("{0:0.00}", AndreAccount.balance)}");
+Console.WriteLine($"\nSaldo da conta da Maria, apos transf. da conta do Andre: R$ {String.Format("{0:0.00}", MariaAccount.balance)}...");
+Console.WriteLine($"...e o saldo da conta do Andre: R$ {String.Format("{0:0.00}", AndreAccount.balance)}");
+
+Console.WriteLine("\nTipo por valor x Tipo por referencia:");
+
+double valuet1 = 300;
+double valuet2 = valuet1;
+Console.WriteLine("\nTipo por valor, atribuindo a variavel: ");
+Console.WriteLine(valuet1==valuet2);
+BankingAccount AndreAccount2 = new BankingAccount();
+AndreAccount2.owner = "André Silva";
+AndreAccount2.agNum = 15;
+AndreAccount2.accNum = "1010-X";
+AndreAccount2.balance = 100;
+Console.Write("\n");
+Console.WriteLine("Tipo por referencia, comparando a variavel, retornando se está na mesma posição da memória:");
+Console.WriteLine(AndreAccount == AndreAccount2);
+
+Console.WriteLine("\n8) Exibir informações de um objeto por um método criado na classe:");
+Console.Write("\nDados da conta do Andre:");
+AndreAccount.ShowAcc(AndreAccount);
+
+/*
+Pelo princípio da responsabilidade, uma classe deve tratar especificamente
+de um tema somente, ter uma responsabilidade única.
+*/
+
+
+
+Console.Write("\n");
 // closing...
-Console.WriteLine("\n\nPress any key to continue...");
-Console.ReadLine();
+//Console.WriteLine("\n\nPress any key to continue...");
+//Console.ReadLine();
