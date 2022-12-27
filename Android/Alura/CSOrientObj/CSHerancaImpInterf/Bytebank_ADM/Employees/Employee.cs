72c58936c164ca9dbe79ebbd7179324a3adaa81f
jjmattos1 <brainstorm@jonasmattos.com.br>
2022-12-27 08:18:17 -0300

    Atualizando repo Android.

---
+++ Android/Alura/CSOrientObj/CSHerancaImpInterf/Bytebank_ADM/Employees/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//using Bytebank_ADM;
+
+namespace Bytebank_ADM
+{
+    public class Employee 
+    {         
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public double Wage { get; set; }
+        
+        public double GetBonus()
+        {
+        	return this.Wage *= 0.1;
+        }
+        
+        
+        
+    }
+}
---
+++ Android/Alura/CSOrientObj/CSHerancaImpInterf/Bytebank_ADM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+//using Bytebank_ADM.Employee;
+
+namespace Bytebank_ADM
+{
+	public class Program
+	{
+		public static void Main()
+		//static void Main(string[] args)
+		{
+			Console.WriteLine("Alura");
+			Console.WriteLine("> C#: Usando herança e implementando interfaces\n");
+			Console.WriteLine("1) Employee class and method overload.\n\n");
+			
+			Console.WriteLine("...");
+			
+			Employee id0 = new Employee();
+			id0.Name = "Pedro";
+			id0.Cpf = "0123456789";
+			id0.Wage = 2500;
+			Console.WriteLine("\n");
+			Console.WriteLine("Bônus salarial do "+id0.Name+": "+id0.GetBonus());
+			
+			
+			
+		}
+	}
+}
