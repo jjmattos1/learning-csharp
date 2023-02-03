f8255d8518664868fa029c5dec53f196223b895f
jjmattos1 <brainstorm@jonasmattos.com.br>
2023-02-03 08:39:14 -0300

    Atualizando repo Android.

---
+++ Android/Alura/CSOrientObj/CSharpClassesStructs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpClassesStructs
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			// https://www.alura.com.br/artigos/c-sharp-entendendo-classes-e-structs
+			
+			Console.WriteLine("/| Alura /|\n"
+								+"\nC#: entendendo classes e structs\n");
+			
+			
+			
+			
+
+		}
+	}
+}
--- Android/Alura/CSOrientObj/CsharpRegex/Program.cs
+++ Android/Alura/CSOrientObj/CsharpRegex/Program.cs
@@ -25,6 +25,11 @@ namespace CsharpRegex
 			Console.Write("\nDigite um valor para validar se é numérico: ");
 			string toValidate = Console.ReadLine();
 			
+			/*
+			Usando o .IsMatch()
+			Com o IsMatch podemos validar se um valor informado combina com a expressão
+			regular definida. O retorno do método é um booleano e recebe como parâmetro uma string.
+			*/
 			bool validation = Regex.IsMatch(toValidate,"^[0-9]+$");
 			if (!validation)
 			{
@@ -35,7 +40,29 @@ namespace CsharpRegex
 				Console.WriteLine("\nO valor informado é numérico!");
 			}
 			
+			Console.Write("\nDigite um CPF: ");
+			var cpf = Console.ReadLine();
 			
+			/*
+			Usando o .Match()
+			Este método pode ser usado para buscar uma string ou
+			expressão regex definida na classe.
+			*/
+			
+			Regex regexCpfMask = new Regex(@"([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})", RegexOptions.IgnoreCase);
+			var cpfFormatValid = regexCpfMask.Match(cpf);
+			if (cpfFormatValid.Success)
+			{
+				Console.WriteLine("\nO formato do CPF esta válido!");
+			}
+			else
+			{
+				Console.WriteLine("\nO formato do CPF esta inválido!");
+			}
+			
+			// https://www.alura.com.br/artigos/javascript-replace-manipulando-strings-e-regex
+			// https://www.alura.com.br/conteudo/expressoes-regulares
+			// https://docs.microsoft.com/pt-br/dotnet/api/system.text.regularexpressions.regex?view=net-6.0
 		}
 	}
 }
